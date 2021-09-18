94c17f0f2be6ca27d7cf0b955f6dbfcc49d2a755
Leandro Oliveira <leandroti.oliveira@gmail.com>
2021-09-18 00:10:05 -0300

    feat: add state robot

--- robots/text.js
+++ robots/text.js
@@ -14,12 +14,18 @@ const nlu = new NaturalLanguageUnderstandingV1({
   serviceUrl: watsonApiCredentials.url,
 });
 
-async function robot(content) {
+const state = require('./state.js');
+
+async function robot() {
+  const content = state.load();
+
   await fetchContentFromWikipedia(content);
   sanitizeContent(content);
   breakContentIntoSentences(content);
   limitMaximumSentences(content);
   await fetchKeywordsOfAllSentences(content);
+
+  state.save(content);
 
   async function fetchContentFromWikipedia(content) {
     const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey);
